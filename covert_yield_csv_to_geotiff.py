5309a75c93c3e1ea416eef389100583a9c2e1aad
1rah <irah.wajcman@gmail.com>
2018-07-02 16:30:36 +1000

    yield image creator

--- covert_yield_csv_to_geotiff.py
+++ covert_yield_csv_to_geotiff.py
@@ -22,20 +22,44 @@ import affine
 from skimage.transform import resize
 #from sklearn.metrics import r2_score
 import argparse
+import math 
 
-
+def interpolate_df_to_img_grid(df, new_img_array):
+    rs,cs = new_img_array.shape
+    ci = np.arange(cs)
+    ri = np.arange(rs)
+    c,r,z = df.col, df.row, df['yield']
+    zi = griddata((c, r), z, (ci[None,:], ri[:,None]), method='linear')
+    return zi
 
 
 if __name__ == '__main__':
     csvfile = r'D:\Steve_SA_winter_2017 Yield Data\sample.csv'
+    gsd = 20
     
     #open csv
     df = pd.read_csv(csvfile)
     
-    #normalise get UTM
-    
+    #normalise to 255 get UTM (return mapping in metadata)
+    max_yield = df['yield'].max()
+    min_yield = df['yield'].min()
+    df['px'] = ((df['yield']-min_yield) / max_yield)*255
+
+    left, right = df['easting'].min(), df['easting'].max()
+    top, bottom = df['northing'].min(), df['northing'].max()
     
     #generate grid
+    width = right-left
+    height = bottom-top
+    
+    cols = np.arange(math.ceil(width/20))
+    rows = np.arange(math.ceil(height/20))
+    
+    
     
     #get min, max boundaries
     #calc affine from gsd and top left corner
+    
+    
+    tif = r'D:\Yield-Map-Correlation\Terraidi_NDRE_rasters\20180212T001111_T55JGH_S2A_ndre_gray.float.tif'
+    img = rio.open(tif)
