bc546e1efe529327f397eee43232e34281a8a5e3
1rah <irah.wajcman@gmail.com>
2018-07-03 18:31:35 +1000

    now writing file to disk

--- covert_yield_csv_to_geotiff.py
+++ covert_yield_csv_to_geotiff.py
@@ -29,6 +29,7 @@ import math
 
 if __name__ == '__main__':
     csvfile = r'D:\Steve_SA_winter_2017 Yield Data\sample2.csv'
+    out_tiff = r'D:\Steve_SA_winter_2017 Yield Data\test.tiff'
     gsd = 10
     
     #open csv
@@ -65,6 +66,49 @@ if __name__ == '__main__':
     plt.scatter(df['row'],df['col'])
     plt.imshow(zi)
 
+    epsg_code = df['epsg_code'][0]
+    zi_height, zi_width = zi.shape
+    def norm(a):
+        a = np.where(np.isnan(a),0,a)
+        return np.array(((a-a.min())/a.max())*255, dtype=np.uint8)
+    zi = norm(zi)    
+    
+    profile = dict()
+    
+    profile.update({
+            'driver': 'GTiff',
+            'dtype': 'uint8',
+            'nodata': 0,
+            'width': zi_width,
+            'height': zi_height,
+            'count': 1,
+            'crs':rio.crs.CRS.from_epsg(epsg_code),
+            'affine':a,
+            'transform':a.to_gdal(),
+            #add min and max for reverse transform
+            })
+    
+    with rio.open(out_tiff, "w", **profile) as dest:
+        dest.write(zi, 1)
+
     tif_path = r'D:\Steve_SA_winter_2017 Yield Data'
     tif_path += r'\20171121T004659_T54HTJ_S2B_ndre_gray.tif'
     img = rio.open(tif_path)
+
+"""
+{'driver': 'GTiff',
+ 'dtype': 'uint8',
+ 'nodata': None,
+ 'width': 800,
+ 'height': 514,
+ 'count': 1,
+ 'crs': CRS({'init': 'epsg:32754'}),
+ 'transform': (277258.338817,
+  1.5750000000000868,
+  0.0,
+  6344313.490408,
+  0.0,
+  -1.5758754863812432),
+ 'affine': Affine(1.5750000000000868, 0.0, 277258.338817,
+        0.0, -1.5758754863812432, 6344313.490408)}
+ """
